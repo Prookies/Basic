56bdade429c206e051a286d9a47d30a8b958ea6c
xuhua <13631784452@163.com>
2021-01-09 17:01:21 +0800

    用数组实现二叉树

--- DataStructure/BinaryTree/BinaryTree_array.cpp
+++ DataStructure/BinaryTree/BinaryTree_array.cpp
@@ -10,178 +10,192 @@
 #define TRUE 1
 #define FALSE 0
 
-#define MAXSIZE 100
-#define MAX_TREE_SIZE 100
+const int MaxSize = 100;
+const int MaxTreeSize = 100;
 
 typedef int Status;
-typedef int TElemType;
-typedef TElemType SqBiTree[MAX_TREE_SIZE];
 
 struct Position
 {
     int level, order;
 };
 
-TElemType Nil = 0;
-
-Status visit(TElemType c)
+template <typename T>
+class BinaryTree
 {
-    printf("%d ", c);
-    return OK;
-}
+public:
+    typedef T TElemType;
 
-Status InitBiTree(SqBiTree T)
-{
-    int i;
-    for (i = 0; i < MAX_TREE_SIZE; i++)
-        T[i] = Nil;
-    return OK;
-}
+    BinaryTree(int size = MaxTreeSize) : mSize(size), pData(new TElemType[size]), Nil(0) {}
 
-Status CreateBiTree(SqBiTree T)
-{
-    int i = 0;
-    printf("按层序输入节点的值（整形），0表示空节点，输入999结束，节点数≤%d：\n", MAX_TREE_SIZE);
-    while (i < 10)
+    ~BinaryTree()
     {
-        T[i] = i + 1;
-        if (i != 0 && T[(i + 1) / 2 - 1] == Nil && T[i] != Nil)
+        if (pData)
         {
-            printf("出现无双亲的非根节点%d\n", T[i]);
-            exit(ERROR);
+            delete[] pData;
         }
-        i++;
     }
-    while (i < MAX_TREE_SIZE)
+
+    Status Create()
     {
-        T[i] = Nil;
-        i++;
-    }
-    return OK;
-}
-
-#define ClearBiTree InitBiTree
-
-Status BiTreeEmpty(SqBiTree T)
-{
-    if (T[0] == Nil)
-        return TRUE;
-    else
-        return FALSE;
-}
-
-int BiTreeDepth(SqBiTree T)
-{
-    int i, j = -1;
-    for (i = MAX_TREE_SIZE - 1; i >= 0; i--)
-    {
-        if (T[i] != Nil)
-            break;
-    }
-    i++;
-
-    do
-    {
-        j++;
-    } while (i >= powl(2, j));
-    return j;
-}
-
-Status Root(SqBiTree T, TElemType *e)
-{
-    if (BiTreeEmpty(T))
-        return ERROR;
-    else
-    {
-        *e = T[0];
+        int i = 0;
+        printf("按层序输入节点的值（整形），0表示空节点，输入999结束，节点数≤%d：\n", MaxTreeSize);
+        while (i < 10)
+        {
+            pData[i] = i + 1;
+            if (i != 0 && pData[(i + 1) / 2 - 1] == Nil && pData[i] != Nil)
+            {
+                printf("出现无双亲的非根节点%d\n", pData[i]);
+                exit(ERROR);
+            }
+            i++;
+        }
+        while (i < mSize)
+        {
+            pData[i] = Nil;
+            i++;
+        }
         return OK;
     }
-}
 
-TElemType Value(SqBiTree T, Position e)
-{
-    return T[(int)powl(2, e.level - 1) + e.order - 2];
-}
-
-Status Assign(SqBiTree T, Position e, TElemType value)
-{
-    int i = (int)powl(2, e.level - 1) + e.order - 2;
-    if (value != Nil && T[(i + 1) / 2 - 1] == Nil)
-        return ERROR;
-    else if (value == Nil && (T[i * 2 + 1] != Nil || T[i * 2 + 2] != Nil))
-        return ERROR;
-    T[i] = value;
-    return OK;
-}
-
-TElemType Parent(SqBiTree T, TElemType e)
-{
-    int i;
-    if (T[0] == Nil)
-        return Nil;
-    for (i = 1; i <= MAX_TREE_SIZE - 1; i++)
+    Status visit(TElemType c)
     {
-        if (T[i] == e)
-            return T[(i + 1) / 2 - 1];
+        printf("%d ", c);
+        return OK;
     }
-    return Nil;
-}
 
-TElemType LeftChild(SqBiTree T, TElemType e)
-{
-    int i;
-    if (T[0] == Nil)
-        return Nil;
-    for (i = 0; i <= MAX_TREE_SIZE - 1; i++)
+    Status Init()
     {
-        if (T[i] == e)
-            return T[i * 2 + 1];
+        for (int i = 0; i < mSize; i++)
+            pData[i] = Nil;
+        return OK;
     }
-    return Nil;
-}
 
-TElemType RightChild(SqBiTree T, TElemType e)
-{
-    int i;
-    if (T[0] == Nil)
-        return Nil;
-    for (i = 0; i <= MAX_TREE_SIZE - 1; i++)
+    Status Empty()
     {
-        if (T[i] == e)
-            return T[i * 2 + 2];
+        if (pData[0] == Nil)
+            return TRUE;
+        else
+            return FALSE;
     }
-    return Nil;
-}
 
-TElemType LeftSibling(SqBiTree T, TElemType e)
-{
-    int i;
-    if (T[0] == Nil)
-        return Nil;
-    for (i = 1; i <= MAX_TREE_SIZE - 1; i++)
+    int Depth()
     {
-        if (T[i] == e && i % 2 == 0)
-            return T[i - 1];
-    }
-    return Nil;
-}
+        int i, j = -1;
+        for (i = mSize - 1; i >= 0; i--)
+        {
+            if (pData[i] != Nil)
+                break;
+        }
+        i++;
 
-TElemType RightSibling(SqBiTree T, TElemType e)
-{
-    int i;
-    if (T[0] == Nil)
-        return Nil;
-    for (i = 1; i <= MAX_TREE_SIZE - 1; i++)
+        do
+        {
+            j++;
+        } while (i >= powl(2, j));
+        return j;
+    }
+
+    void Clear()
     {
-        if (T[i] == e && i % 2)
-            return T[i + 1];
+        Init();
     }
-    return Nil;
-}
 
-// 遍历存在数组越界的情况
-void PreTraverse(SqBiTree T, int e)
-{
+    Status Root(TElemType *e)
+    {
+        if (Empty())
+            return ERROR;
+        else
+        {
+            *e = pData[0];
+            return OK;
+        }
+    }
+
+    TElemType Value(Position e)
+    {
+        return pData[(int)powl(2, e.level - 1) + e.order - 2];
+    }
+
+    Status Assign(Position e, TElemType value)
+    {
+        int i = (int)powl(2, e.level - 1) + e.order - 2;
+        if (value != Nil && pData[(i + 1) / 2 - 1] == Nil)
+            return ERROR;
+        else if (value == Nil && (pData[i * 2 + 1] != Nil || pData[i * 2 + 2] != Nil))
+            return ERROR;
+        pData[i] = value;
+        return OK;
+    }
+
+    TElemType Parent(TElemType e)
+    {
+        int i;
+        if (pData[0] == Nil)
+            return Nil;
+        for (i = 1; i <= mSize - 1; i++)
+        {
+            if (pData[i] == e)
+                return pData[(i + 1) / 2 - 1];
+        }
+        return Nil;
+    }
+
+    TElemType LeftChild(TElemType e)
+    {
+        int i;
+        if (pData[0] == Nil)
+            return Nil;
+        for (i = 0; i <= mSize - 1; i++)
+        {
+            if (pData[i] == e)
+                return pData[i * 2 + 1];
+        }
+        return Nil;
+    }
+
+    TElemType RightChild(TElemType e)
+    {
+        int i;
+        if (pData[0] == Nil)
+            return Nil;
+        for (i = 0; i <= mSize - 1; i++)
+        {
+            if (pData[i] == e)
+                return pData[i * 2 + 2];
+        }
+        return Nil;
+    }
+
+    TElemType LeftSibling(TElemType e)
+    {
+        int i;
+        if (pData[0] == Nil)
+            return Nil;
+        for (i = 1; i <= mSize - 1; i++)
+        {
+            if (pData[i] == e && i % 2 == 0)
+                return pData[i - 1];
+        }
+        return Nil;
+    }
+
+    TElemType RightSibling(TElemType e)
+    {
+        int i;
+        if (pData[0] == Nil)
+            return Nil;
+        for (i = 1; i <= mSize - 1; i++)
+        {
+            if (pData[i] == e && i % 2)
+                return pData[i + 1];
+        }
+        return Nil;
+    }
+
+    // 遍历存在数组越界的情况
+    void PreTraverse(int e)
+    {
 #if 0
     visit(T[e]);
     if (T[2 * e + 1] != Nil)
@@ -189,73 +203,73 @@ void PreTraverse(SqBiTree T, int e)
     if (T[2 * e + 2] != Nil)
         PreTraverse(T, 2 * e + 2);
 #else
-    if (T[e] == Nil)
-        return;
+        if (pData[e] == Nil)
+            return;
 
-    visit(T[e]);
-    PreTraverse(T, 2 * e + 1);
-    PreTraverse(T, 2 * e + 2);
+        visit(pData[e]);
+        PreTraverse(2 * e + 1);
+        PreTraverse(2 * e + 2);
 
 #endif
-}
-
-Status PreOrderTraverse(SqBiTree T)
-{
-    if (!BiTreeEmpty(T))
-        PreTraverse(T, 0);
-    printf("\n");
-    return OK;
-}
-
-void InTraverse(SqBiTree T, int e)
-{
-    if (T[2 * e + 1] != Nil)
-        InTraverse(T, 2 * e + 1);
-    visit(T[e]);
-    if (T[2 * e + 2] != Nil)
-        InTraverse(T, 2 * e + 2);
-}
-
-Status InOrderTraverse(SqBiTree T)
-{
-    if (!BiTreeEmpty(T))
-        InTraverse(T, 0);
-    printf("\n");
-    return OK;
-}
-
-void PostTraverse(SqBiTree T, int e)
-{
-    if (T[2 * e + 1] != Nil)
-        PostTraverse(T, 2 * e + 1);
-    if (T[2 * e + 2] != Nil)
-        PostTraverse(T, 2 * e + 2);
-    visit(T[e]);
-}
-
-Status PostOrderTraverse(SqBiTree T)
-{
-    if (!BiTreeEmpty(T))
-        PostTraverse(T, 0);
-    printf("\n");
-    return OK;
-}
-
-void LevelOrderTraverse(SqBiTree T)
-{
-    int i = MAX_TREE_SIZE - 1, j;
-    while (T[i] == Nil)
-        i--;
-    for (j = 0; j <= i; j++)
-    {
-        if (T[j] != Nil)
-            visit(T[j]);
     }
-    printf("\n");
-}
 
-void Print(SqBiTree T)
-{
+    Status PreOrderTraverse()
+    {
+        if (!Empty())
+            PreTraverse(0);
+        printf("\n");
+        return OK;
+    }
+
+    void InTraverse(int e)
+    {
+        if (pData[2 * e + 1] != Nil)
+            InTraverse(2 * e + 1);
+        visit(pData[e]);
+        if (pData[2 * e + 2] != Nil)
+            InTraverse(2 * e + 2);
+    }
+
+    Status InOrderTraverse()
+    {
+        if (!Empty())
+            InTraverse(0);
+        printf("\n");
+        return OK;
+    }
+
+    void PostTraverse(int e)
+    {
+        if (pData[2 * e + 1] != Nil)
+            PostTraverse(2 * e + 1);
+        if (pData[2 * e + 2] != Nil)
+            PostTraverse(2 * e + 2);
+        visit(pData[e]);
+    }
+
+    Status PostOrderTraverse()
+    {
+        if (!Empty())
+            PostTraverse(0);
+        printf("\n");
+        return OK;
+    }
+
+    void LevelOrderTraverse()
+    {
+        int i = MaxTreeSize - 1, j;
+        while (pData[i] == Nil)
+            i--;
+        for (j = 0; j <= i; j++)
+        {
+            if (pData[j] != Nil)
+                visit(pData[j]);
+        }
+        printf("\n");
+    }
+
+    void Print()
+    {
 #if 0
     int j, k;
     Position p;
@@ -274,63 +288,69 @@ void Print(SqBiTree T)
         printf("\n");
     }
 #else
-    int i = 0, j = 0;
-    while (T[i] != Nil)
-    {
-        int num = (int)powl(2, j);
-        printf("第%d层：", j);
-        int count = 0;
-        while (count < num)
+        int i = 0, j = 0;
+        while (pData[i] != Nil)
         {
-            printf("%d:%d ", count++, T[i++]);
+            int num = (int)powl(2, j);
+            printf("第%d层：", j);
+            int count = 0;
+            while (count < num)
+            {
+                printf("%d:%d ", count++, pData[i++]);
+            }
+            printf("\n");
+            j++;
         }
-        printf("\n");
-        j++;
-    }
 #endif
-}
+    }
+
+private:
+    int mSize;
+    TElemType *pData;
+    TElemType Nil;
+};
 
 int main()
 {
+    BinaryTree<int> tree;
     Status i;
     Position p;
-    TElemType e;
-    SqBiTree T;
-    InitBiTree(T);
-    CreateBiTree(T);
+    int e;
+    tree.Init();
+    tree.Create();
     printf("建立二叉树后， 树空否？%d(1:是 0:否 树的深度=%d\n",
-           BiTreeEmpty(T), BiTreeDepth(T));
-    i = Root(T, &e);
+           tree.Empty(), tree.Depth());
+    i = tree.Root(&e);
     if (i)
         printf("二叉树的根为：%d\n", e);
     else
         printf("树空，无根\n");
     printf("打印二叉树\n");
-    Print(T);
+    tree.Print();
     printf("层序遍历二叉树：\n");
-    LevelOrderTraverse(T);
+    tree.LevelOrderTraverse();
     printf("前序遍历二叉树：\n");
-    PreOrderTraverse(T);
+    tree.PreOrderTraverse();
     printf("中序遍历二叉树：\n");
-    InOrderTraverse(T);
+    tree.InOrderTraverse();
     printf("后序遍历二叉树：\n");
-    PostOrderTraverse(T);
+    tree.PostOrderTraverse();
     printf("修改节点的层号3本层序号2。");
     p.level = 3;
     p.order = 2;
-    e = Value(T, p);
+    e = tree.Value(p);
     printf("待修改节点的原值为%d请输入新值：50", e);
     e = 50;
-    Assign(T, p, e);
+    tree.Assign(p, e);
     printf("前序遍历二叉树：\n");
-    PreOrderTraverse(T);
-    printf("节点%d的双亲为%d，左右孩子分别为", e, Parent(T, e));
-    printf("%d, %d, 左右兄弟分别为", LeftChild(T, e), RightChild(T, e));
-    printf("%d, %d\n", LeftSibling(T, e), RightSibling(T, e));
-    ClearBiTree(T);
+    tree.PreOrderTraverse();
+    printf("节点%d的双亲为%d，左右孩子分别为", e, tree.Parent(e));
+    printf("%d, %d, 左右兄弟分别为", tree.LeftChild(e), tree.RightChild(e));
+    printf("%d, %d\n", tree.LeftSibling(e), tree.RightSibling(e));
+    tree.Clear();
     printf("清除二叉树，树空否？%d(1:是 0:否) 树的深度=%d\n",
-           BiTreeEmpty(T), BiTreeDepth(T));
-    i = Root(T, &e);
+           tree.Empty(), tree.Depth());
+    i = tree.Root(&e);
     if (i)
         printf("二叉树的根为：%d\n", e);
     else
