650ebee0ffebb4128f5b1607abc2a3c87dcdf96e
xuhua <13631784452@163.com>
2021-01-31 18:20:18 +0800

    测试switch的底层实现

---
+++ CPlusPlus/test_switch/test_switch.cpp
@@ -0,0 +1,20 @@
+int main()
+{
+    int x = 1;
+
+    switch (x)
+    {
+    case 1:
+        x += 1;
+        break;
+
+    case 2:
+        x *= 10;
+        break;
+
+    default:
+        break;
+    }
+
+    return 0;
+}
